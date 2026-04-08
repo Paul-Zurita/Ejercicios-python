702323693c3e8add4b2ffa6cfd4193b51eef63d6
Paul-Zurita <e11864@ism.edu.ec>
2026-04-08 12:52:22 -0500

    actividad 1

---
+++ ACTIVIDAD 1.py
@@ -0,0 +1,34 @@
+#ACTIVIDAD 1
+#Paúl Zurita - Ruta 1 - 08/04/2026
+suma = 3 + 4
+resta = 3 - 4   
+modulo = 3 % 4
+division = 3 / 4
+multiplicacion = 3 * 4
+potencia = 3 ** 4
+suelo = 3//4
+
+print ("La suma de 3 y 4 es: ",suma)
+print ("La resta de 3 y 4 es: ",resta)
+print ("El modulo de 3 y 4 es: ",modulo)
+print ("La division de 3 y 4 es: ",division)
+print ("La multiplicacion de 3 y 4 es: ",multiplicacion)
+print ("La potencia de 3 y 4 es: ",potencia)
+print ("La division de suelo de 3 y 4 es: ",suelo)
+
+#-----------------------------------------------------------
+
+print ("Paul")
+print ("Zurita")
+print ("Ecuador")
+print ("Estoy disfrutando python")
+
+#---------------------------------------------------------
+print (type (10))
+print (type (9.8))
+print (type (3.14))
+print (type ("Asabeneh"))
+print (type ("Paul") )
+print (type ("Zurita"))  
+print (type ("Ecuador"))
+print (type ("Estoy disfrutando python"))
--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 print ("variables")
 #comentar antes de cualquier parte compleja
-int (edad= 4)
-float(peso = 3.24)
-str (texto = "pitehuevo")
+edad = 4
+peso = 3.24
+
