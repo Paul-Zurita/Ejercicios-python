fe816db41feab0f3e5935774eee451ff336938de
Paul-Zurita <e11864@ism.edu.ec>
2026-04-08 13:24:22 -0500

    Actividad 2

---
+++ #Actividad 2_variables.py
@@ -0,0 +1,11 @@
+#Actividad 2_variables
+#08-04-2026
+primer_nobre, primer_apellido, pais, edad, casado, = "Paul" , "Zurita" , "Ecuador", 17, False
+
+print (primer_nobre,primer_apellido,pais,edad,casado)
+print ("primer nombre:", primer_nobre)
+print ("primer apellido:", primer_apellido)
+print ("Pais:", pais)
+print ("edad:", edad)
+print ("Casado?:", casado)
+
