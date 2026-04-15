278222df3d7bf2297e3580ed57fed55e57d9ce57
Paul-Zurita <e11864@ism.edu.ec>
2026-04-15 13:25:31 -0500

    actividad de desempacatar

--- actividades vrs.py
+++ actividades vrs.py
@@ -1,6 +1,33 @@
+#%%
 nombre =  'paul'
 a,b,c,d = 'paul' #desempaquetar
 print (a) 
 print (b)
 print (c)
 print (d)
+#%%
+first_letter = nombre[0]
+print(first_letter) # P
+second_letter = nombre[1]
+print(second_letter) # y
+last_index = len(nombre) - 1
+last_letter = nombre[last_index]
+print(last_letter) # n
+#%%
+name = "paul"
+first_letter = name[0]
+print(first_letter)  # p
+second_letter = name[1]
+print(second_letter)  # a
+third_letter = name[2]
+print(third_letter)  # u
+last_index = len(name) - 1
+last_letter = name[last_index]
+print(last_letter)  # l
+print(name[0:2])  # pa
+print(name[-1])  # l
+print(name[-3])  # a
+print(name[-3:])  # aul
+print(name[::2])    # pu
+print(name[::-1])  # luap
+# %%
