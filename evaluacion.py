808022a045869f37dd34f7aa3528786f9a8442c5
Paul-Zurita <e11864@ism.edu.ec>
2026-04-20 09:27:41 -0500

    ACTIVIDAD A

---
+++ evaluacion.py
@@ -0,0 +1,27 @@
+# ===== PARTE A =====
+# Respuesta 1:
+#Observa el siguiente código:
+nombre = "Lucía"
+edad = 16
+promedio = 9.75
+cursos = ["Python", "HTML", "CSS"]
+print(type(nombre))
+print(type(edad))
+print(type(promedio))
+print(type(cursos))
+print(len(nombre))
+#a) Indica el tipo de dato de cada variable.
+#Respuestas:
+#nombre -> str
+#edad -> int
+#promedio -> float
+#cursos-> lista e str
+#b) Escribe qué mostraría el programa en pantalla.
+#c) Explica qué hace len(nombre).
+
+# ===== PARTE B =====
+# Código corregido
+# ...
+# ===== PARTE C =====
+# Programa integrador
+# ...
