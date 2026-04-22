2d32290add15ef70ec3c9973e3c0e7d4ce8bbf39
Paul-Zurita <e11864@ism.edu.ec>
2026-04-22 13:20:26 -0500

    condicionales pochos

--- ACTIVIDAD 5  CONDICIONALES.py
+++ ACTIVIDAD 5  CONDICIONALES.py
@@ -1,12 +1,41 @@
 a = int(input("dame un numero: "))
-if a > 0:
-    print ("a es positivo")
-    if a % 2 == 0:
-        print("Par positivo")
-    elif a % 2 != 0:
-        print ("par negativo")
+if a > 0 and a % 2 == 0:
+    print ("a es positivo y par")
+elif a > 0:
+        print ("impar positivo")
+if a < 0 and a % 2 == 0:
+    print ("a es negativo y par")
 elif a < 0:
-    print ("a es negativo")
+        print ("impar negativo")
 else:
     print('a es cero')
 
+b = 0
+if b > 0:
+    if b % 2 == 0:
+        print('b es positivo par')
+    else:
+        print('b es positivo')
+elif b == 0:
+    print('b es cero')
+else:
+    print('b es negativo')
+#%%
+c = int(input("dame otro numero: "))
+if c == 0:
+    print ("es cero")
+else:
+    if c > 0:
+        if c % 2 == 0:
+            print("es positivo par")
+        else : 
+            print ("positivo impar")
+            if c < 0:
+                if c % 2 == 0:
+                    print ("negativo par")
+                else: 
+                    print ("negativo impar")
+#%%
+edad = int(input("edad? "))
+print ("Mayor de edad") if edad >=18 else print ("menor de edad :(")
+# %%
