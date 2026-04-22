3c1ef3d44968389842d86dc68ffffe7f24e5f66e
Paul-Zurita <e11864@ism.edu.ec>
2026-04-22 12:25:03 -0500

    aritoooo

--- ACTIVIDAD 5  CONDICIONALES.py
+++ ACTIVIDAD 5  CONDICIONALES.py
@@ -1,3 +1,12 @@
-a=3
+a = int(input("dame un numero: "))
 if a > 0:
     print ("a es positivo")
+    if a % 2 == 0:
+        print("Par positivo")
+    elif a % 2 != 0:
+        print ("par negativo")
+elif a < 0:
+    print ("a es negativo")
+else:
+    print('a es cero')
+
