1a51daafc322186e74a6402dc91b9822682971e8
Paul-Zurita <e11864@ism.edu.ec>
2026-05-11 10:59:22 -0500

    Caso Práctico: Limpiando Datos

---
+++ Caso Práctico Limpiando Datos.py
@@ -0,0 +1,11 @@
+cedula = input("coloca tu cedula :) ")
+
+cedula_limpia = ""
+
+for caracter in cedula:
+    if caracter == '-' or caracter == " ":
+        continue
+    
+    cedula_limpia = cedula_limpia + caracter
+
+print(cedula_limpia)
