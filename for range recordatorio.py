dc784ff980b03910f5835a890a7f6e7b8b9ea8bc
Paul-Zurita <e11864@ism.edu.ec>
2026-05-18 10:53:11 -0500

    AcTIVIDAD HECHA :)

--- for range recordatorio.py
+++ for range recordatorio.py
@@ -1,8 +1,9 @@
 suma = 0
 num_nota = 1
 promedio = int(input("di el numero de notas: "))
-for num in range (promedio):
-    nota = float(input(f"nota numero {num_nota+1}, coloque su nota: "))
-    suma += nota
+if promedio < 0 and promedio> 2:
+    for num in range (promedio):
+        nota = float(input(f"nota numero {num_nota+1}, coloque su nota: "))
+        suma += nota
 promedio_total = suma/promedio
 print(f"tu promedio de notas es {promedio_total}")
