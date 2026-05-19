721651bd6e31a10d701aa108c3e47adedf1572f5
Paul-Zurita <e11864@ism.edu.ec>
2026-05-19 18:37:01 -0500

    actividad completa :)

--- lista de for.py
+++ lista de for.py
@@ -1,5 +1,9 @@
 notas = [10,8,9,7,6,5,4,3,2,1]
+
 cuantas = int(input("Di el numero de notas que salgan: "))
-if cuantas> 0 and cuantas < 11:
+
+if cuantas > 0 and cuantas < 11:
     for nota in range(cuantas):
-        print(f"Nota registrada{notas[cuantas]}")
+        print(f"Nota registrada: {notas[nota]}")
+else:
+    print("error, te crees muy listo????")
