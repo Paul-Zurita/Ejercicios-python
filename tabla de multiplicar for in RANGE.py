a7d7b07ccd846385ac78059d0b462b0ddfbec21a
Paul-Zurita <e11864@ism.edu.ec>
2026-05-20 12:36:43 -0500

    tabla ahora para par

--- tabla de multiplicar for in RANGE.py
+++ tabla de multiplicar for in RANGE.py
@@ -2,7 +2,7 @@ num = int(input("di un numero que quieres multiplicar: "))
 ini = int(input("des que numero quieres multiplicar: "))
 i = int(input("hasta que numero quieres multiplicar: "))
 if ini < i:
-    for i in range (ini,i+1):
+    for i in range (ini+1,i,2):
         print (f"{i} x {num} = {i*num}")
 else:
     print ("multiplicacion no valida")
