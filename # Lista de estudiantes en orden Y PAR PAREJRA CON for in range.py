e0475384a83dc4d10b42ee9c441fb6c9d4e94b7e
Paul-Zurita <e11864@ism.edu.ec>
2026-05-20 12:57:14 -0500

    trabajos

---
+++ # Lista de estudiantes en orden Y PAR PAREJRA CON for in range.py
@@ -0,0 +1,5 @@
+estudiantes = ["Ana", "Luis", "María", "Carlos", "Sofía", "Mateo"]
+num_pareja = 1
+for i in range (0,6,2):
+    print (f"pareja {num_pareja} {estudiantes[i]},{estudiantes[i+1]}" )
+    num_pareja += 1
