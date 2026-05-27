9129e13dde508f5ee9651dcb702f7a8146740b53
Paul-Zurita <e11864@ism.edu.ec>
2026-05-27 12:22:50 -0500

    clave para ingresaar

---
+++ Evaluaación Final.py
@@ -0,0 +1,11 @@
+clave_correcta = "python123"
+clave = ""
+
+while clave != clave_correcta:
+    clave = input("Ingrese la clave: ")
+
+    if clave != clave_correcta:
+        print("Clave incorrecta")
+
+print("Acceso permitido")
+print("Bienvenido al sistema")9
