661a1d4a74760acc501f0f3d73f6a35a253a2205
Paul-Zurita <e11864@ism.edu.ec>
2026-05-27 12:46:21 -0500

    solicitud de datos

--- Evaluaación Final.py
+++ Evaluaación Final.py
@@ -10,4 +10,20 @@ while clave != clave_correcta:
 print("Acceso permitido")
 print("Bienvenido al sistema")
 temas = ["variables","cálculos","input","print","f-string","condicionales","ciclos"]
+
 print (temas)
+
+for tema in temas:
+    print(tema)
+
+cantidad = int(input("\n¿Cuántos estudiantes serán revisados?: "))
+
+for i in range(cantidad):
+
+    print(f"\nEstudiante {i + 1}")
+
+nombre = input("Nombre: ")
+ejercicios = float(input("Nota de ejercicios básicos: "))
+condicionales = float(input("Nota de condicionales: "))
+ciclos = float(input("Nota de ciclos: "))
+practicas = int(input("Cantidad de prácticas completadas: "))
