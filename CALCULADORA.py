cb822de3cfbed8d1e6dc85d45593348ad021f868
Paul-Zurita <e11864@ism.edu.ec>
2026-06-03 13:01:11 -0500

    profe cambio todo

--- CALCULADORA.py
+++ CALCULADORA.py
@@ -15,22 +15,29 @@ def division(a, b):
 
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
+while True:
+    print("------SELECCIONE UNA OPERACION------")
+    print("1. Suma (+)")
+    print("2. Resta (-)")
+    print("3. Multiplicación (*)")
+    print("4. División (/)")
+    print("5. Salir")
+    opcion = int(input("Selecciona una de la opciones (1-5): "))
+    if opcion == 1:
+        resultado = suma(num1, num2)
+        print("Resultado:", resultado)
+    elif opcion == 2:
+        resultado = resta(num1, num2)
+        print("Resultado:", resultado)
+    elif opcion == 3:
+        resultado = multiplicacion(num1, num2)
+        print("Resultado:", resultado)
+    elif opcion == 4:
+        resultado = division(num1, num2)
+        print("Resultado:", resultado)
+    elif opcion ==5:
+        print("gracias por usa la calcu")
+        break
+    else:
+        resultado = "Opción no válida"
 
-print("------SELECCIONE UNA OPERACION------")
-print("1. Suma (+)")
-print("2. Resta (-)")
-print("3. Multiplicación (*)")
-print("4. División (/)")
-opcion = int(input("Selecciona una de la opciones (1-4): "))
-if opcion == 1:
-    resultado = suma(num1, num2)
-elif opcion == 2:
-    resultado = resta(num1, num2)
-elif opcion == 3:
-    resultado = multiplicacion(num1, num2)
-elif opcion == 4:
-    resultado = division(num1, num2)
-else:
-    resultado = "Opción no válida"
-
-print("Resultado:", resultado)
