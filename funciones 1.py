e85719f3aa5f71d9650e2db787edc29c060a34bc
Paul-Zurita <e11864@ism.edu.ec>
2026-06-05 07:59:41 -0500

    Ordenadno el repositorio

--- funciones 1.py
+++ funciones 1.py
@@ -1,13 +1,17 @@
 #%%
 
-#%%
+def mostrar_estudiante(nombre, curso):
+    print("=== DATOS DEL ESTUDIANTE ===")
+    print(f"Nombre: {nombre}")
+    print(f"Curso: {curso}")
+
 i = 0
-cantidad = int(input("Cuantos alumnos deseas ingresar: "))
-while i < cantidad + 1:
-    def mostrar_estudiante (nombre,curso):
-    print ("=== DATOS DEL ESTUDIANTE ===")
-    print (f"Nombre : {nombre}")
-    print (f"curso: {curso}")
-nombre_usuario= input("Ingrese el nombre del estudiante: ")
-curso_usuario= input("Ingrese el curso del estudiante: ")
-mostrar_estudiante (nombre_usuario, curso_usuario)
+cantidad = int(input("¿Cuántos alumnos deseas ingresar?: "))
+
+while i < cantidad:
+    nombre_usuario = input("Ingrese el nombre del estudiante: ")
+    curso_usuario = input("Ingrese el curso del estudiante: ")
+
+    mostrar_estudiante(nombre_usuario, curso_usuario)
+
+    i += 1
